bbad02ab52a90457f51e1cf231db6e67859e3275
greffgreff <greffchandler80@gmail.com>
2023-09-30 03:12:20 +0200

    Removed app nav and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import AppContainer from './lifecycle/AppContainer'
-import AppNavigator from './lifecycle/AppNavigator'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import OnBoardingScreen from './screens/OnBoardingScreen'
+import useAppFonts from './hooks/use-app-fonts'
+
+const { Navigator, Screen } = createNativeStackNavigator()
 
 export default () => {
+  const { loaded } = useAppFonts()
+
   return (
-    <AppContainer>
-      <AppNavigator />
-    </AppContainer>
+    loaded && (
+      <NavigationContainer>
+        <Navigator initialRouteName='OnBoardingScreen'>
+          <Screen name='OnBoardingScreen' options={{ headerShown: false }}>
+            {() => <OnBoardingScreen onCompletion={console.log} />}
+          </Screen>
+        </Navigator>
+      </NavigationContainer>
+    )
   )
 }
--- src/lifecycle/AppContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react'
-import useAppFonts from '../hooks/use-app-fonts'
-
-export default (props: PropsWithChildren) => {
-  const { loaded } = useAppFonts()
-  return loaded ? props.children : null
-}
