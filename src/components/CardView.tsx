4bda3c62bedf0ff87753b3e9e22ea2c43749ce03
greffgreff <greffchandler80@gmail.com>
2023-10-02 00:53:19 +0200

    Worked on cards

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,7 +1,5 @@
-import React, { Component, PropsWithChildren } from 'react'
-import { StyleSheet, View, Text, ViewStyle } from 'react-native'
-import { FONTS } from '../constants'
-import DropShadow from './DropShadow'
+import React, { createRef, Component, PropsWithChildren, useRef } from 'react'
+import { StyleSheet, View, ViewStyle, Animated, Easing, Dimensions } from 'react-native'
 
 interface CardViewProps extends PropsWithChildren {
   cardCount?: number
@@ -10,32 +8,51 @@ interface CardViewProps extends PropsWithChildren {
   direction?: 'top' | 'left' | 'bottom' | 'right'
 }
 
+const AnimatedView = Animated.createAnimatedComponent(View)
+const screen = Dimensions.get('screen')
+
 class CardView extends Component<CardViewProps> {
   private cardCount = React.Children.count(this.props.children)
 
   state = {
     cardIndex: 0,
     selectionAmount: 3,
+    positionState: new Animated.ValueXY({ x: 0, y: 0 }),
+    hiddenPositionState: new Animated.Value(0),
   }
 
   flipNext = () => {
-    this.flipCard(this.state.cardIndex + 1)
+    if (this.state.cardIndex < this.cardCount) {
+      this.setState({ cardIndex: this.state.cardIndex + 1 })
+    }
   }
 
   flipPrevious = () => {
-    this.flipCard(this.state.cardIndex - 1)
+    if (this.state.cardIndex > 0) {
+      this.setState({ cardIndex: this.state.cardIndex - 1 })
+    }
   }
 
-  flipTo = (index: number) => {
-    this.flipCard(index)
+  private moveCardOut = () => {
+    const { positionState } = this.state
+    Animated.timing(positionState, {
+      toValue: { x: -screen.width, y: 0 },
+      duration: 400,
+      easing: Easing.ease,
+      useNativeDriver: true,
+    }).start()
   }
 
-  private flipCard = (index: number) => {
-    // implement flip animation
+  componentDidUpdate(prevProps: any) {
+    console.log(this.state.cardIndex, prevProps)
+    if (this.state.cardIndex !== prevProps.cardIndex) {
+      this.moveCardOut()
+    }
   }
 
   render() {
     const { children } = this.props
+    const { positionState, cardIndex } = this.state
 
     const startIndex = Math.max(0, this.state.cardIndex - this.state.selectionAmount)
     const endIndex = Math.min(this.cardCount - 1, this.state.cardIndex + this.state.selectionAmount - 1)
@@ -43,27 +60,25 @@ class CardView extends Component<CardViewProps> {
     return (
       <View style={styles.container}>
         {React.Children.map(children, (child, index) => {
-          const isSelected = index >= startIndex && index <= endIndex
+          // const isSelected = index >= startIndex && index <= endIndex
 
-          if (isSelected) {
-            const background: ViewStyle = {
-              backgroundColor: `rgba(0, 0, 0, ${0.05 * index})`,
-            }
-
-            const modification: ViewStyle = {
-              zIndex: -index,
-              // transform:
-              // index == 0 ? [] : [{ rotateZ: `${getBetween(5, -5)}deg` }, { translateY: index * 2 }],
-              transform: [{ translateY: index * 30 }, { scale: 1 - index * 0.1 }],
-            }
-
-            return (
-              <View key={index} style={[styles.card, modification]}>
-                {child}
-                <View style={[styles.card, background]} />
-              </View>
-            )
+          const modification: ViewStyle = {
+            zIndex: -index,
+            transform: [{ translateX: positionState.x }],
+            // transform:
+            // index == 0 ? [] : [{ rotateZ: `${getBetween(5, -5)}deg` }, { translateY: index * 2 }],
+            // transform:
+            //   index <= cardIndex
+            //     ? [{ translateX: positionState.x }]
+            //     : [{ translateY: index * 30 }, { scale: 1 - index * 0.1 }],
           }
+
+          return (
+            <AnimatedView key={index} style={[styles.card, modification]}>
+              {child}
+              <View style={[styles.card, { backgroundColor: `rgba(0, 0, 0, ${0.05 * index})` }]} />
+            </AnimatedView>
+          )
         })}
       </View>
     )
