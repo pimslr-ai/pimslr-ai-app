bcf62dac82f8a96e69113228f2e39be8d09e203a
greffgreff <greffchandler80@gmail.com>
2023-10-12 10:52:20 +0200

    Added audio file creation and deletion

--- src/screens/SpeechTest.tsx
+++ src/screens/SpeechTest.tsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from 'react'
 import { View, Text, Button } from 'react-native'
 import { Audio } from 'expo-av'
+import * as FileSystem from 'expo-file-system'
 
 export default () => {
   const [recording, setRecording] = useState<Audio.Recording | null>(null)
+  const [audioRecording, setAudioRecording] = useState<string | null>(null)
 
   useEffect(() => {
     Audio.requestPermissionsAsync()
   }, [])
 
+  useEffect(() => {
+    if (audioRecording) {
+      console.log(audioRecording)
+      FileSystem.deleteAsync(audioRecording)
+    }
+  }, [audioRecording])
+
   const startRecording = async () => {
+    setAudioRecording(null)
     await Audio.setAudioModeAsync({ allowsRecordingIOS: true, playsInSilentModeIOS: true })
     const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY)
     setRecording(recording)
@@ -19,7 +29,8 @@ export default () => {
     setRecording(null)
     await recording?.stopAndUnloadAsync()
     await Audio.setAudioModeAsync({ allowsRecordingIOS: false })
-    const uri = recording?.getURI()
+    const uri = recording?.getURI()!
+    setAudioRecording(uri)
     console.log(uri)
   }
 
