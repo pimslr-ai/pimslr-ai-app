2c477cfbce9f47a8362f1337ac3268262448add2
greffgreff <greffchandler80@gmail.com>
2023-10-16 11:19:22 +0200

    Integrated useSpeech

--- src/screens/SpeechTest.tsx
+++ src/screens/SpeechTest.tsx
@@ -1,51 +1,21 @@
-import { useEffect, useState } from 'react'
 import { View, Text, Button } from 'react-native'
-import { Audio } from 'expo-av'
-import * as FileSystem from 'expo-file-system'
+import useSpeech from '../hooks/useSpeech'
 
 export default () => {
-  const [recording, setRecording] = useState<Audio.Recording | null>(null)
-  const [audioRecording, setAudioRecording] = useState<string | null>(null)
-
-  useEffect(() => {
-    Audio.requestPermissionsAsync()
-  }, [])
-
-  useEffect(() => {
-    if (audioRecording) {
-      console.log(audioRecording)
-      FileSystem.deleteAsync(audioRecording)
-    }
-  }, [audioRecording])
-
-  const startRecording = async () => {
-    setAudioRecording(null)
-    await Audio.setAudioModeAsync({ allowsRecordingIOS: true, playsInSilentModeIOS: true })
-    const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY)
-    setRecording(recording)
-  }
-
-  const stopRecording = async () => {
-    setRecording(null)
-    await recording?.stopAndUnloadAsync()
-    await Audio.setAudioModeAsync({ allowsRecordingIOS: false })
-    const uri = recording?.getURI()!
-    setAudioRecording(uri)
-    console.log(uri)
-  }
+  const { startRecording, stopRecording, audioTranscript, isRecording } = useSpeech('en-US')
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
       <Text style={{ textAlign: 'center', fontSize: 20, paddingVertical: 20 }}>
-        {recording ? 'Listening...' : 'Start listening.'}
+        {isRecording ? 'Listening...' : audioTranscript ? audioTranscript : 'Waiting for input'}
       </Text>
 
       <View
         style={{ display: 'flex', paddingVertical: 20, flexDirection: 'row', justifyContent: 'space-evenly' }}
       >
         <Button
-          title={recording ? 'Stop recording' : 'Start recording'}
-          onPress={recording ? stopRecording : startRecording}
+          title={isRecording ? 'Stop recording' : 'Start recording'}
+          onPress={isRecording ? stopRecording : startRecording}
         />
       </View>
     </View>
