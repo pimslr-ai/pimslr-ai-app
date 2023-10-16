a0ee453113a94bd866c8b76b2b46a82943c1c74c
greffgreff <greffchandler80@gmail.com>
2023-10-17 00:27:14 +0200

    Simplified state management

--- src/hooks/useMic.tsx
+++ src/hooks/useMic.tsx
@@ -2,27 +2,36 @@ import { useEffect, useState } from 'react'
 import { Audio } from 'expo-av'
 
 export default () => {
-  const [recording, setRecording] = useState<Audio.Recording | null>(null)
-  const [audioRecording, setAudioRecording] = useState<string | null>(null)
+  const [state, setState] = useState<{
+    recording?: Audio.Recording
+    uri?: string
+  }>({})
 
   useEffect(() => {
     Audio.requestPermissionsAsync()
   }, [])
 
   const startRecording = async () => {
-    setAudioRecording(null)
-    await Audio.setAudioModeAsync({ allowsRecordingIOS: true, playsInSilentModeIOS: true })
-    const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY)
-    setRecording(recording)
+    if (!state.recording) {
+      await Audio.setAudioModeAsync({ allowsRecordingIOS: true, playsInSilentModeIOS: true })
+      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY)
+      setState({ recording, uri: undefined })
+    }
   }
 
   const stopRecording = async () => {
-    setRecording(null)
-    await recording?.stopAndUnloadAsync()
-    await Audio.setAudioModeAsync({ allowsRecordingIOS: false })
-    const uri = recording?.getURI()
-    setAudioRecording(uri!)
+    if (state.recording) {
+      await state.recording.stopAndUnloadAsync()
+      await Audio.setAudioModeAsync({ allowsRecordingIOS: false })
+      const uri = state.recording?.getURI()!
+      setState({ uri, recording: undefined })
+    }
   }
 
-  return { startRecording, stopRecording, audioRecording, isRecording: recording !== null }
+  return {
+    startRecording,
+    stopRecording,
+    audioRecording: state.uri,
+    isRecording: !!state.recording,
+  }
 }
