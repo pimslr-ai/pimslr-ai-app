e85abbf6a17b788d13fff916f311b10311f576ea
greffgreff <greffchandler80@gmail.com>
2023-10-19 01:29:13 +0200

    added useMic and useSpeech hooks

---
+++ src/hooks/useMic.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Audio } from 'expo-av'
+
+export default () => {
+  const [state, setState] = useState<{
+    recording?: Audio.Recording
+    uri?: string
+  }>({})
+
+  useEffect(() => {
+    Audio.requestPermissionsAsync()
+  }, [])
+
+  const startRecording = async () => {
+    if (!state.recording) {
+      await Audio.setAudioModeAsync({ allowsRecordingIOS: true, playsInSilentModeIOS: true })
+      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY)
+      setState({ recording, uri: undefined })
+    }
+  }
+
+  const stopRecording = async () => {
+    if (state.recording) {
+      await state.recording.stopAndUnloadAsync()
+      await Audio.setAudioModeAsync({ allowsRecordingIOS: false })
+      const uri = state.recording?.getURI()!
+      setState({ uri, recording: undefined })
+    }
+  }
+
+  return {
+    startRecording,
+    stopRecording,
+    audioRecording: state.uri,
+    isRecording: !!state.recording,
+  }
+}
---
+++ src/hooks/useSpeech.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import useMic from './useMic'
+import * as FileSystem from 'expo-file-system'
+import axios from 'axios'
+
+export default (language: string) => {
+  const { startRecording, stopRecording, audioRecording, isRecording } = useMic()
+  const [state, setState] = useState<{
+    recognition?: Recognition | null
+    isLoading: boolean
+    hasFailed?: boolean
+  }>({ isLoading: false })
+
+  useEffect(() => {
+    if (audioRecording) {
+      setState({ recognition: null, isLoading: true })
+
+      speechToText(audioRecording)
+        .then(data => setState({ recognition: data, isLoading: false, hasFailed: data === null }))
+        .finally(() => FileSystem.deleteAsync(audioRecording!))
+    }
+  }, [audioRecording])
+
+  const speechToText = async (audioFile: string) => {
+    const audio = await FileSystem.readAsStringAsync(audioFile, {
+      encoding: FileSystem.EncodingType.Base64,
+    })
+    const url = 'http://pimslrai.greffchandler.net/speech/recognize/' + language
+    const response = await axios.post<RecognizeResponse>(url, { audio })
+    return response.data.results.length ? response.data.results[0]!.alternatives[0]! : null
+  }
+
+  return {
+    startRecording,
+    stopRecording,
+    isRecording,
+    isLoading: state.isLoading,
+    recognition: state.recognition,
+    hasFailed: state.hasFailed,
+  }
+}
