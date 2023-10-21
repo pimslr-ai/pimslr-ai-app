e93b97d5f771e5c52bc5f59003cbbc497d10772d
greffgreff <greffchandler80@gmail.com>
2023-10-21 17:30:53 +0200

    Added status check

--- src/components/ScreenView.tsx
+++ src/components/ScreenView.tsx
@@ -1,9 +1,15 @@
 import { PropsWithChildren } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { THEME } from '../constants'
+import { StatusBar } from 'expo-status-bar'
 
 export default ({ children }: PropsWithChildren) => {
-  return <View style={styles.body}>{children}</View>
+  return (
+    <View style={styles.body}>
+      <StatusBar style='auto' />
+      {children}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
