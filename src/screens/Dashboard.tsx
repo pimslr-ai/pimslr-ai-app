3f2e83916e5a88a23fa5c837617990722e5de111
greffgreff <greffchandler80@gmail.com>
2023-10-29 18:35:04 +0100

    Worked on dashboard course sections

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { FONTS, TEST_COURSE } from '../constants'
-import { StyleSheet, Text, TouchableOpacity, ViewStyle } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, ViewStyle } from 'react-native'
 import { PropsWithChildren } from 'react'
 import { useNavigation } from '.'
 import SectionView from '../components/SectionView'
@@ -42,16 +42,22 @@ export default () => {
       </SectionView>
 
       <SectionView name='Courses'>
-        <Card>
+        {/* <Card>
           <Text>Create new course</Text>
-        </Card>
-
+        </Card> */}
+        {courses?.map(course => (
+          <CourseCard key={course.id} {...course} />
+        ))}
         {courses?.map(course => (
           <CourseCard key={course.id} {...course} />
         ))}
       </SectionView>
 
-      <SectionView name='Saved sentences' />
+      <SectionView name='Saved sentences'>
+        <Card>
+          <Text>This is a saved sentence.</Text>
+        </Card>
+      </SectionView>
     </ScreenView>
   )
 }
@@ -71,38 +77,86 @@ const Card = ({ children, onClick, style }: CardProps) => {
 
 const CourseCard = (course: Course) => {
   const navigation = useNavigation()
+  const backgroundCards = new Array(3).fill(null)
+
+  const backgroundCardStyle = (index: number): ViewStyle => ({
+    width: '100%',
+    height: '100%',
+    backgroundColor: `rgba(0, 0, 0, ${0.03 * (index + 1)})`,
+    transform: [{ rotateZ: `${getBetween(7, -7)}deg` }],
+    margin: 20,
+    position: 'absolute',
+    borderRadius: 20,
+    zIndex: -index,
+  })
 
   return (
-    <Card onClick={() => navigation.navigate('course:home', { courseId: course.id })}>
-      <Text style={styles.courseDate}>{formatHumanDateTime(course.createdAt)}</Text>
-      <Text style={styles.courseTitle}>{course.scenario.title}</Text>
-      <Text style={styles.courseDetails}>{course.language.name} | 85% native speaker | 6/10 completed</Text>
-    </Card>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('course:home', { courseId: course.id })}
+      style={{ padding: 20, overflow: 'visible', position: 'relative' }}
+    >
+      <View style={styles.courseCard}>
+        <Text style={styles.courseTitle}>{course.scenario.title}</Text>
+        <Text style={styles.courseDate}>{formatHumanDateTime(course.createdAt)}</Text>
+        <Text style={styles.courseDetails}>{course.language.name} | 85% native speaker | 6/10 completed</Text>
+      </View>
+      {backgroundCards.map((_, i) => (
+        <View key={i} style={backgroundCardStyle(i)} />
+      ))}
+    </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   cardContainer: {
-    padding: 20,
+    display: 'flex',
+    gap: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 40,
     borderRadius: 10,
     borderColor: 'rgba(0,0,0,0.05)',
     borderWidth: 1.5,
     minHeight: 100,
+  },
+  courseCard: {
     display: 'flex',
     gap: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 60,
+    borderRadius: 20,
+    width: '100%',
+    minHeight: 100,
+    backgroundColor: 'white',
+    zIndex: 9,
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
   },
   courseTitle: {
     fontSize: 24,
     fontFamily: FONTS.POPPINS.MEDIUM,
   },
   courseDate: {
-    fontSize: 12,
+    fontSize: 10,
   },
   courseDetails: {},
+  courseCardStackContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
 
 function formatHumanDateTime(date: Date) {
   const datePart = date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })
   const timePart = date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' })
   return `${datePart} | ${timePart}`
+}
+
+const getBetween = (num1: number, num2: number) => {
+  if (num1 > num2) {
+    ;[num1, num2] = [num2, num1]
+  }
+  return Math.random() * (num2 - num1) + num1
 }
