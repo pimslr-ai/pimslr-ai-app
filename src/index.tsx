11fba020a0ad3912651757b1bf145487d1665bbb
greffgreff <greffchandler80@gmail.com>
2023-11-30 16:14:05 +0100

    Removed redundant storage provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
 import { registerRootComponent } from 'expo'
 import App from './App'
-import StorageProvider from './contexts/StorageProvider'
 
-registerRootComponent(() => (
-  <StorageProvider>
-    <App />
-  </StorageProvider>
-))
+registerRootComponent(() => <App />)
