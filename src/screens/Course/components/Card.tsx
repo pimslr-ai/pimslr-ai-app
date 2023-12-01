739a50921b5ad9f4f4c76ee060b7ffb86779d340
greffgreff <greffchandler80@gmail.com>
2023-12-01 19:53:50 +0100

    Made use of `forwardRef`

--- src/screens/Course/components/Card.tsx
+++ src/screens/Course/components/Card.tsx
@@ -1,121 +1,108 @@
-import { View, Text, StyleSheet } from 'react-native'
 import { FONTS } from '../../../constants'
-import { Assessment, Sentence } from '../../../types'
+import { View, Text, StyleSheet } from 'react-native'
+import { Sentence } from '../../../types'
+import useAudio from '../../../hooks/useAudio'
+import usePronunciation from '../../../hooks/usePronunciation'
+import SecondaryButton from '../../../components/SecondaryButton'
+import { Ref, forwardRef, useEffect, useImperativeHandle } from 'react'
 
 interface CardProps {
+  language: string
   sentence: Sentence
-  isCurrent: boolean
-  assessment?: Partial<Assessment>
   onSuccess?: () => void
+  onSound?: (isPlaying: boolean) => void
+  onRecording?: (isRecording: boolean) => void
+  onAssessment?: (isAssessing: boolean) => void
 }
 
-export default ({ sentence, isCurrent, assessment, onSuccess }: CardProps) => {
+export interface CardRef {
+  toggleRecording: () => void
+  toggleSound: () => void
+}
+
+const Card = ({ language, sentence, onSound, onRecording, onAssessment }: CardProps, ref: Ref<CardRef>) => {
+  console.log(sentence.sentence, ref)
+  const { toggleSound, isPlaying } = useAudio(sentence.voice.audio)
+  const { toggleRecording, isRecording, isAssessing, assessment } = usePronunciation(
+    language,
+    sentence.sentence,
+  )
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      toggleRecording,
+      toggleSound,
+    }),
+    [],
+  )
+
+  useEffect(() => {
+    if (onSound) {
+      onSound(isPlaying)
+    }
+  }, [isPlaying])
+
+  useEffect(() => {
+    if (onRecording) {
+      onRecording(isRecording)
+    }
+  }, [isRecording])
+
+  useEffect(() => {
+    if (onAssessment) {
+      onAssessment(isAssessing)
+    }
+  }, [isAssessing])
+
+  useEffect(() => {
+    if (assessment) {
+      console.log(JSON.stringify(assessment, null, 2))
+    }
+  }, [assessment])
+
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
-        <Text style={styles.translation}>
-          <Sentence
-            onSuccess={onSuccess!}
-            isCurrent={isCurrent}
-            translation={sentence?.sentence}
-            assessment={assessment!}
+        <View style={styles.content}>
+          <SecondaryButton
+            label='Record'
+            labelStyle={{ color: isRecording ? 'red' : 'black' }}
+            onClick={toggleRecording}
           />
-        </Text>
-        <Text style={styles.original}>{sentence.english}</Text>
+          <SecondaryButton
+            label='Audio'
+            labelStyle={{ color: isPlaying ? 'red' : 'black' }}
+            onClick={toggleSound}
+          />
+          <Text style={styles.translation}>{sentence.sentence}</Text>
+          <Text style={styles.original}>{sentence.english}</Text>
+        </View>
       </View>
     </View>
   )
 }
 
-interface SentenceProps {
-  isCurrent: boolean
-  translation: string
-  assessment?: Partial<Assessment>
-  onSuccess?: () => void
-}
-
-const Sentence = ({ translation, isCurrent, assessment, onSuccess }: SentenceProps) => {
-  if (assessment && isCurrent) {
-    const strip = (input: string) => {
-      return input
-        .replace(/[.,\/#!$%\^&\*;:{}=\\?_`~()]/g, '')
-        .replace(/\s+/g, ' ')
-        .toLowerCase()
-    }
-
-    function isMatch(arr1: string[], arr2: string[]) {
-      const sortedArr1 = arr1.slice().sort()
-      const sortedArr2 = arr2.slice().sort()
-      return sortedArr1.every((value, index) => value === sortedArr2[index])
-    }
-
-    function getColor(confidence: number): string {
-      const colors = [
-        'rgb(252, 54, 17)',
-        'rgb(252, 122, 19)',
-        'rgb(247, 169, 40)',
-        'rgb(247, 169, 40)',
-        'rgb(156, 167, 40)',
-        'rgb(156, 167, 40)',
-        'rgb(89, 168, 60)',
-        'rgb(89, 168, 60)',
-      ]
-      const index = Math.floor(confidence * (colors.length - 1))
-      return colors[index]
-    }
-
-    const originalWords = translation.split(' ').map(w => ({ word: w, stripped: strip(w) }))
-    const scoredWords = assessment?.words?.map(w => ({ word: strip(w.word), score: w.accuracyScore }))
-
-    const matched = isMatch(
-      originalWords.filter(w => w.stripped !== '').map(w => w.stripped),
-      scoredWords?.map(w => w.word)!,
-    )
-
-    if (matched) {
-      onSuccess!()
-    }
-
-    return originalWords.map((original, i) => {
-      let confidenceColor = 'black'
-
-      if (original.stripped !== '') {
-        const matchingWord = scoredWords?.filter(w => w.word === original.stripped)[0]
-
-        if (matchingWord === undefined) {
-          confidenceColor = 'rgb(252, 54, 17)'
-        } else {
-          confidenceColor = getColor(matchingWord?.score!)
-        }
-      }
-
-      return (
-        <Text key={i} style={{ color: confidenceColor }}>
-          {original.word + ' '}
-        </Text>
-      )
-    })
-  }
-
-  return <Text>{translation}</Text>
-}
+export default forwardRef<CardRef, CardProps>(Card)
 
 const styles = StyleSheet.create({
   container: {
-    shadowOffset: { width: 0, height: 10 },
-    shadowOpacity: 0.1,
-    shadowRadius: 20,
-    padding: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 32,
   },
   wrapper: {
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.1,
+    shadowRadius: 15,
     elevation: 14,
-    borderRadius: 20,
-    width: '100%',
     backgroundColor: 'white',
-    overflow: 'hidden',
+    width: '100%',
+    borderRadius: 24,
+  },
+  content: {
+    minHeight: 250,
     paddingVertical: 70,
     paddingHorizontal: 32,
-    minHeight: 250,
     justifyContent: 'center',
     alignContent: 'center',
   },
