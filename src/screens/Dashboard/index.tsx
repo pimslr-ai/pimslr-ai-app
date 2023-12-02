2ff22b6399fb6e2e0c8e1e91416745e41e24b859
greffgreff <greffchandler80@gmail.com>
2023-12-02 05:05:16 +0100

    Cleaned up Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,15 @@
-import { View, ScrollView } from 'react-native'
+import { View, ScrollView, Text } from 'react-native'
 import SectionView from '../../components/SectionView'
 import ScreenView from '../../components/ScreenView'
 import Logo from '../../components/Logo'
 import CourseCard from './components/CourseCard'
 import { styles } from './style'
+import { Course } from '../../types'
+import { useNavigation } from '..'
+import { course } from '../Course/test'
 
 export default () => {
-  // const { courses } = useCourses()
+  const courses: Course[] = [course]
 
   return (
     <ScreenView>
@@ -16,50 +19,14 @@ export default () => {
           {/* <Button icon='settings' label='Settings' /> */}
         </View>
 
-        {/* <SectionView name='Languages' extra='Add language'>
-          <View style={{ marginHorizontal: 16, gap: 16 }}>
-            <Card>
-              <Text>French</Text>
-            </Card>
-            <Card>
-              <Text>Dutch</Text>
-            </Card>
-            <Card>
-              <Text>Spanish</Text>
-            </Card>
-          </View>
-          <Button label='View all' labelStyle={{ marginTop: 16, textAlign: 'center', width: '100%' }} />
-        </SectionView> */}
-
-        {/* <SectionView name='Scenarios' extra='Add scenario'>
-          <View style={{ marginHorizontal: 16, gap: 16 }}>
-            <Card>
-              <Text>You are at a bar...</Text>
-            </Card>
-            <Card>
-              <Text>You are at a carwash...</Text>
-            </Card>
-          </View>
-          <Button label='View all' labelStyle={{ marginTop: 16, textAlign: 'center', width: '100%' }} />
-        </SectionView> */}
-
         <SectionView name='Courses'>
           <View style={{ padding: 16, gap: 16 }}>
-            {courses?.map(course => (
-              <CourseCard key={course.id} {...course} />
+            {courses?.map((course, i) => (
+              <CourseCard key={i} course={course} />
             ))}
             {/* <Button label='View all' labelStyle={{ marginTop: 16, textAlign: 'center', width: '100%' }} /> */}
           </View>
         </SectionView>
-
-        {/* <SectionView name='Saved sentences'>
-          <View style={{ padding: 16, gap: 16 }}>
-            <Card>
-              <Text>This is a saved sentence.</Text>
-            </Card>
-          </View>
-          <Button label='View all' labelStyle={{ marginTop: 16, textAlign: 'center', width: '100%' }} />
-        </SectionView> */}
       </ScrollView>
     </ScreenView>
   )
