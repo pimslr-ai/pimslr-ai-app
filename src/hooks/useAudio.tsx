da730806d419b0b9ca69f7f59bff1d85db9faf2c
greffgreff <greffchandler80@gmail.com>
2023-12-02 01:47:15 +0100

    Added `isLoading` state

--- src/hooks/useAudio.tsx
+++ src/hooks/useAudio.tsx
@@ -2,14 +2,15 @@ import { useEffect, useRef, useState } from 'react'
 import { AVPlaybackStatusSuccess, Audio } from 'expo-av'
 import { cacheDirectory, writeAsStringAsync } from 'expo-file-system'
 
-export default (base64: string) => {
+export default () => {
   const sound = useRef(new Audio.Sound()).current
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     Audio.requestPermissionsAsync().then(() => {
       Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
+        allowsRecordingIOS: false,
         playsInSilentModeIOS: true,
       })
     })
@@ -23,14 +24,15 @@ export default (base64: string) => {
     })
   }, [])
 
-  useEffect(() => {
-    loadSound()
-  }, [base64])
-
-  const loadSound = async () => {
+  const loadSound = async (base64: string) => {
+    console.log('Loading sound...')
+    setIsPlaying(false)
+    setIsLoading(true)
     const temporaryFile = `${cacheDirectory}_voice.m4a`
     await writeAsStringAsync(temporaryFile, base64, { encoding: 'base64' })
+    await sound.unloadAsync()
     await sound.loadAsync({ uri: temporaryFile })
+    setIsLoading(false)
   }
 
   const playSound = async () => {
@@ -48,20 +50,36 @@ export default (base64: string) => {
   }
 
   const toggleSound = async () => {
-    setIsPlaying(isPlaying => {
-      if (isPlaying) {
-        sound.stopAsync()
-      } else {
-        sound.replayAsync()
-      }
-      return !isPlaying
-    })
+    if (isPlaying) {
+      console.log('Stopping sound...')
+      setIsPlaying(false)
+      await sound.stopAsync()
+    } else {
+      console.log('Playing sound...')
+      setIsPlaying(true)
+      await sound.replayAsync()
+    }
   }
 
+  // const toggleSound = async () => {
+  //   setIsPlaying(isPlaying => {
+  //     if (isPlaying) {
+  //       console.log('Stopping sound...')
+  //       sound.stopAsync()
+  //     } else {
+  //       console.log('Playing sound...')
+  //       sound.replayAsync()
+  //     }
+  //     return !isPlaying
+  //   })
+  // }
+
   return {
+    loadSound,
     toggleSound,
     playSound,
     stopSound,
     isPlaying,
+    isLoading,
   }
 }
